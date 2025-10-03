1a68a536357b650bc3dd95883f103ba9c1f66d74
Atikin <a@vokmok.ru>
2025-10-03 14:51:01 +0300

    Add structure

---
+++ include/entities/crans/Cran.h
@@ -0,0 +1,10 @@
+//
+// Created by Никита Комков on 03.10.2025.
+//
+
+#ifndef CARGO_CRANS_CRAN_H
+#define CARGO_CRANS_CRAN_H
+namespace Cran {
+
+}
+#endif //CARGO_CRANS_CRAN_H
---
+++ include/entities/ships/Ship.h
@@ -0,0 +1,10 @@
+//
+// Created by Никита Комков on 03.10.2025.
+//
+
+#ifndef CARGO_CRANS_SHIP_H
+#define CARGO_CRANS_SHIP_H
+namespace Ship {
+
+}
+#endif //CARGO_CRANS_SHIP_H
