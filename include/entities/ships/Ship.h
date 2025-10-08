e4b0bbb608c08f3f768052922cf1cc015886eefa
Nerey <saburovyar.2008@mail.ru>
2025-10-08 15:18:48 +0300

    delete  deviation from ship

--- include/entities/ships/Ship.h
+++ include/entities/ships/Ship.h
@@ -10,18 +10,12 @@ namespace Ship {
         std::string name_;
         Utils::ShipTypes type_;
         Utils::weight_t cargoWeight_;
-        Utils::time_t arrivalSchedule_;
-        Utils::time_t arrivalReal_;
         Ship(std::string name,
                    Utils::ShipTypes type,
-                   Utils::weight_t cargoWeight,
-                   Utils::time_t arrivalSchedule,
-                   const Deviations::Deviations& deviations);
+                   Utils::weight_t cargoWeight);
         std::string getName() const;
         Utils::ShipTypes getType() const;
         Utils::weight_t getCargoWeight() const;
-        Utils::time_t getArrivalTime() const;
-        Utils::time_t getDeviation() const;
         void unload(Utils::weight_t cargoWeight);
     };
 }
