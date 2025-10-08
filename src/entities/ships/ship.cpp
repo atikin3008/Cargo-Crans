c8af1e9d032d8d11ec04a18ff73a080a1ce54928
Nerey <saburovyar.2008@mail.ru>
2025-10-08 16:03:36 +0300

    change format Ship::Ship

--- src/entities/ships/ship.cpp
+++ src/entities/ships/ship.cpp
@@ -1,7 +1,13 @@
 #include "../../../include/entities/ships/Ship.h"
 
 namespace Ship {
-    Ship::Ship(std::string name,
-                   Utils::ShipTypes type,
-                   Utils::weight_t cargoWeight): name_(name), type_(type), cargoWeight_(cargoWeight){}
+    Ship::Ship(
+        std::string name,
+        Utils::ShipTypes type,
+        Utils::weight_t cargoWeight)
+        : name_(name)
+        , type_(type)
+        , cargoWeight_(cargoWeight)
+    {
+    }
 }
